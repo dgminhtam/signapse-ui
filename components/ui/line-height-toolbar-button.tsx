62d108b94e805b1c7d19039fce4c5716cc55b2bc
Duong Minh Tam <35762319+dgminhtam@users.noreply.github.com>
2026-07-17 17:28:43 +0700

    Implement preview plate editor

---
+++ components/ui/line-height-toolbar-button.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import * as React from 'react';
+
+import { LineHeightPlugin } from '@platejs/basic-styles/react';
+import { WrapText } from 'lucide-react';
+import { useEditorRef, useSelectionFragmentProp } from 'platejs/react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+import { ToolbarButton } from './toolbar';
+
+export function LineHeightToolbarButton(
+  props: React.ComponentProps<typeof DropdownMenu>
+) {
+  const editor = useEditorRef();
+  const { defaultNodeValue, validNodeValues: values = [] } =
+    editor.getInjectProps(LineHeightPlugin);
+
+  const value = useSelectionFragmentProp({
+    defaultValue: defaultNodeValue,
+    getProp: (node) => node.lineHeight,
+  });
+
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen} modal={false} {...props}>
+      <DropdownMenuTrigger asChild>
+        <ToolbarButton pressed={open} tooltip="Line height" isDropdown>
+          <WrapText />
+        </ToolbarButton>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="min-w-0" align="start">
+        <DropdownMenuRadioGroup
+          value={value}
+          onValueChange={(newValue) => {
+            editor
+              .getTransforms(LineHeightPlugin)
+              .lineHeight.setNodes(Number(newValue));
+            editor.tf.focus();
+          }}
+        >
+          {values.map((value) => (
+            <DropdownMenuRadioItem
+              key={value}
+              className="min-w-[180px]"
+              value={value}
+            >
+              {value}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
